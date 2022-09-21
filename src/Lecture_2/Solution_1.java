3ad5af8a47c5ed6f775c1a69a0003d3c3750147c
Golobon <golobon@ya.ru>
2022-09-21 22:39:54 +0300

    Commit

--- src/Lecture_2/Solution_1.java
+++ src/Lecture_2/Solution_1.java
@@ -1,4 +1,43 @@
 package Lecture_2;
 
 public class Solution_1 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drinker {
+        public void askForMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcoholic extends Drinker {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    static class BeerLover implements Alcoholic {
+
+        @Override
+        public void askForMore(String message) {
+
+        }
+
+        @Override
+        public void sayThankYou() {
+
+        }
+
+        @Override
+        public boolean isReadyToGoHome() {
+            return READY_TO_GO_HOME;
+        }
+
+        @Override
+        public void sleepOnTheFloor() {
+
+        }
+    }
 }
