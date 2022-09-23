019b8a22ab9f0c108af98cb4cd4214b999f8917e
Golobon <golobon@ya.ru>
2022-09-24 00:14:00 +0300

    Commit

--- src/Lecture_4/AbstractRobot.java
+++ src/Lecture_4/AbstractRobot.java
@@ -1,11 +1,7 @@
 package Lecture_4;
 
 public abstract class AbstractRobot implements Attackable, Defensable {
-
-    private static int hitCount = 1;
-
-
-    abstract String getName();
+    private int hitCount;
 
     public BodyPart attack() {
         BodyPart attackedBodyPart = null;
@@ -17,26 +13,27 @@ public abstract class AbstractRobot implements Attackable, Defensable {
             attackedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 3) {
             attackedBodyPart = BodyPart.LEG;
-        } else if (hitCount == 4) {
+        } else {
             hitCount = 0;
             attackedBodyPart = BodyPart.CHEST;
         }
+
         return attackedBodyPart;
     }
 
     public BodyPart defense() {
         BodyPart defendedBodyPart = null;
-        hitCount = hitCount + 1;
+        hitCount = hitCount + 2;
 
         if (hitCount == 1) {
             defendedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 2) {
             defendedBodyPart = BodyPart.LEG;
         } else if (hitCount == 3) {
-            defendedBodyPart = BodyPart.ARM;
-        } else if (hitCount == 4) {
-            hitCount = 0;
             defendedBodyPart = BodyPart.CHEST;
+        } else {
+            hitCount = 0;
+            defendedBodyPart = BodyPart.ARM;
         }
         return defendedBodyPart;
     }
